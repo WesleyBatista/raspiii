3dc49e83926212b3f3dc4a5210fb99233aaa7c5c
Wesley <wesley@civey.com>
2019-06-05 01:25:28 +0200

    Initial functionality (#1)
    * Update README.md
    
    * Update .gitignore
    
    * Add requirements files
    
    * Add raspiii.service file
    
    * Add main script
    
    * Delete file after upload

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # raspiii
 sync raspberry pictures to AWS S3
+
+
+# Setup
+
+1. `mkdir -p /home/pi/camera_service/images`
+2. `pip3 install -r requirements.txt`
+
+> FYI: we are using python 3.7.3 _(installed with pyenv-installer)_
+
+
+# Dev workflow
+
+```
+pi@raspberrypi2:~ $ rm ~/camera_service/main.py; nano ~/camera_service/main.py
+```
+
+```
+pi@raspberrypi2:~ $ python -u ~/camera_service/main.py
+```
+
+## `systemd` service
+
+
+```
+~/devel/camera on  master! ⌚ 0:44:25
+$ scp raspiii.service pi@raspberrypi2.local:~/
+```
+
+```
+root@raspberrypi2:/home/pi# mv raspiii.service /etc/systemd/system/
+root@raspberrypi2:/home/pi# systemctl enable raspiii
+root@raspberrypi2:/home/pi# systemctl cat raspiii
+root@raspberrypi2:/home/pi# systemctl start raspiii
+root@raspberrypi2:/home/pi# systemctl status raspiii
+● raspiii.service - Raspiii
+   Loaded: loaded (/etc/systemd/system/raspiii.service; enabled; vendor preset: enabled)
+   Active: active (running) since Tue 2019-06-04 23:59:53 BST; 4min 50s ago
+ Main PID: 539 (python)
+   CGroup: /system.slice/raspiii.service
+           ├─ 539 python -u main.py
+           ├─2636 /bin/sh -c raspistill --mode 0 -o /home/pi/camera_service/images/image_20190604T230443.jpg --noprev
+           └─2637 raspistill --mode 0 -o /home/pi/camera_service/images/image_20190604T230443.jpg --nopreview --expos
+
+Jun 05 00:04:26 raspberrypi2 python[539]: > uploaded image_20190604T230425.jpg
+Jun 05 00:04:28 raspberrypi2 python[539]: > uploaded image_20190604T230426.jpg
+Jun 05 00:04:30 raspberrypi2 python[539]: > uploaded image_20190604T230428.jpg
+Jun 05 00:04:32 raspberrypi2 python[539]: > uploaded image_20190604T230430.jpg
+Jun 05 00:04:34 raspberrypi2 python[539]: > uploaded image_20190604T230432.jpg
+Jun 05 00:04:36 raspberrypi2 python[539]: > uploaded image_20190604T230434.jpg
+Jun 05 00:04:37 raspberrypi2 python[539]: > uploaded image_20190604T230436.jpg
+Jun 05 00:04:39 raspberrypi2 python[539]: > uploaded image_20190604T230437.jpg
+```
+
---
+++ main.py
@@ -0,0 +1,81 @@
+import os
+import socket
+from datetime import datetime
+from subprocess import getstatusoutput
+import boto3
+from botocore.exceptions import ClientError
+
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_S3_BUCKET = os.getenv('AWS_S3_BUCKET')
+
+if not (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and AWS_S3_BUCKET):
+    raise Exception('set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and '
+                    'AWS_S3_BUCKET environment vars')
+
+
+def _get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    result = s.getsockname()[0]
+    s.close()
+    return result
+
+
+AWS_S3_CLIENT = boto3.client('s3')
+LOCAL_IP_ADDRESS = _get_ip()
+HOSTNAME = os.uname().nodename
+
+
+def get_metadata():
+    result = {  # noqa
+        'local_ip_address': LOCAL_IP_ADDRESS,
+        'hostname': HOSTNAME,
+        'date_day': datetime.utcnow().strftime('%Y%m%d'),
+        'upload_timestamp': str(int(datetime.utcnow().timestamp() * 1000000)),
+    }
+    return result
+
+
+def delete_file(filepath):
+    error, output = getstatusoutput(f'rm {filepath}')
+    if error:
+        raise OSError(error, output)
+
+
+def take_a_picture():
+    filepath = f"/home/pi/camera_service/images/image_{datetime.utcnow().strftime('%Y%m%dT%H%M%S')}.jpg"
+    command = f"raspistill --mode 0 -o {filepath} --nopreview --exposure sports --timeout 1"
+    error, output = getstatusoutput(command)
+
+    if error:
+        raise OSError(error, output)
+
+    return filepath
+
+
+def upload_aws(filepath):
+    file_basename = os.path.basename(filepath)
+    s3_path = f'{file_basename}'
+    try:
+        AWS_S3_CLIENT.upload_file(filepath, AWS_S3_BUCKET, s3_path,
+            ExtraArgs={'Metadata': get_metadata()})  # noqa
+    except ClientError as e:
+        print(f'> error: {e}')
+        return False
+
+    print(f'> uploaded {s3_path}')
+
+    return s3_path
+
+
+def main():
+    while True:
+        filepath = take_a_picture()
+        upload_aws(filepath)
+        delete_file(filepath)
+
+
+if __name__ == '__main__':
+    main()
